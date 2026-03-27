6ce82427c5c995d42d39fa256f3a219c23ae8b87
ankitkandwal-git <sharmaankit49103@gmail.com>
2026-03-27 19:57:12 +0530

    Linked List

--- Sorting/MergeSort/main.cpp
+++ Sorting/MergeSort/main.cpp
@@ -1,6 +1,46 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int mergeSort(int arr[], int n,int left, int right){
-    
+int mergeSort(int left,int right,vector<int> &arr){
+    if(left>=right) return 0;
+    int mid = left + (right-left)/2;
+    int inv = mergeSort(left,mid,arr);
+    inv += mergeSort(mid+1,right,arr);
+    vector<int> temp;
+    int i=left,j=mid+1;
+    while(i<=mid && j<=right){
+        if(arr[i]<=arr[j]){
+            temp.push_back(arr[i]);
+            i++;
+        }
+        else{
+            temp.push_back(arr[j]);
+            j++;
+            inv += (mid-i+1);
+        }
+    }
+    while(i<=mid){
+        temp.push_back(arr[i]);
+        i++;
+    }
+    while(j<=right){
+        temp.push_back(arr[j]);
+        j++;
+    }
+    for(int k=left;k<=right;k++){
+        arr[k] = temp[k-left];
+    }
+    return inv;
+
+}
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<mergeSort(0,n-1,arr)<<endl;
+    return 0;
 }
